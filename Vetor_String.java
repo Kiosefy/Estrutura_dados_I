095bca4993f43e3d0273f030252cec848d036ad2
Kiosefy <61634865+Kiosefy@users.noreply.github.com>
2025-09-16 21:36:43 -0300

    Create Vetor_String.java

---
+++ Vetor_String.java
@@ -0,0 +1,13 @@
+public class Vetor_String
+{
+	public static void main(String[] args) {
+	    
+		String [] frutas = {"Maçã", "Banana", "Laranja", "Uva"};
+	
+		System.out.println("Primeira posição: " + frutas[0]);
+		
+		frutas[1] = "Morango";
+		
+		System.out.println("Segunda posição: " + frutas[1]);
+	}
+}
