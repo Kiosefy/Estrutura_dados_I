5931d4db3ad17c801e9a3e37f639d5fda7047999
Kiosefy <61634865+Kiosefy@users.noreply.github.com>
2025-09-16 21:34:53 -0300

    Create Vetor_Booleano.java

---
+++ Vetor_Booleano.java
@@ -0,0 +1,19 @@
+public class Vetor_Booleano
+{
+	public static void main(String[] args) {
+	    
+		boolean [] status = new boolean[3];
+		
+		status[0]=true;
+		status[1]=false;
+		status[2]=true;
+		
+		if(status[0] == true){
+		    System.out.println("Está ativo");
+		}
+		else{
+		    System.out.println("Está inativo");
+		}
+
+	}
+}
